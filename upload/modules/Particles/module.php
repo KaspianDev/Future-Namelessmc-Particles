35f0c535b4b48fa04fc350244ee74842a1e3cffc
Kacper Smoliński <34658474+kacperleague9@users.noreply.github.com>
2021-11-29 15:46:42 +0100

    Update module.php

--- upload/modules/Particles/module.php
+++ upload/modules/Particles/module.php
@@ -52,6 +52,7 @@ class Particles_Module extends Module {
 			    width: 100%;
 			    height: 100%;
 			    z-index: 0;
+			    pointer-events: none
 			}
 			.home-header, .masthead {
 				z-index: -1;
